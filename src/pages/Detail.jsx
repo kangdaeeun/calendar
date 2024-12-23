7784b5a17f46691084e5906e6eb3788c9264523e
Daeeun Kang <daeeun123@daum.net>
2024-12-23 12:13:38 +0900

    feat:ui

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const DetailStyle = styled.div`
+  display: flex;
+  background-color: white;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+  label {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+  }
+  button1 {
+    color: white;
+    background-color: blue;
+    border: none;
+  }
+  button2 {
+    color: white;
+    background-color: red;
+    border: none;
+  }
+  button3 {
+    color: white;
+    background-color: gray;
+    border: none;
+  }
+`;
 
 const Detail = () => {
-  return <div>Detail</div>;
+  return (
+    <DetailStyle>
+      <label>
+        날짜
+        <input type="text" placeholder="YY-MM-DD" />
+      </label>
+      <label>
+        항목
+        <input type="text" placeholder="지출 항목" />
+      </label>
+      <label>
+        금액
+        <input type="number" placeholder="지출 금액" />
+      </label>
+      <label>
+        내용
+        <input type="text" placeholder="지출 내용" />
+      </label>
+      <button1>수정</button1>
+      <button2>삭제</button2>
+      <button3><Link to={"/"}>뒤로 가기</Link></button3>
+    </DetailStyle>
+  );
 };
 
 export default Detail;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,93 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Background = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1200px;
+  width: 100%;
+  gap: 20px;
+`;
+
+const Calendar = styled.div`
+  display: flex;
+  background-color: black;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CreateStyle = styled.div`
+  display: flex;
+  background-color: white;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+  label {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+  }
+  button {
+    background-color: skyblue;
+    border: none;
+  }
+`;
+
+const ListStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Background>
+      <Calendar>
+        <button>JAN</button>
+        <button>FEB</button>
+        <button>MAR</button>
+        <button>APR</button>
+        <button>MAY</button>
+        <button>JUN</button>
+        <button>JUL</button>
+        <button>AUG</button>
+        <button>SEP</button>
+        <button>OCT</button>
+        <button>NOV</button>
+        <button>DEC</button>
+      </Calendar>
+      <CreateStyle>
+        <label>
+          날짜
+          <input type="text" placeholder="YY-MM-DD" />
+        </label>
+        <label>
+          항목
+          <input type="text" placeholder="지출 항목" />
+        </label>
+        <label>
+          금액
+          <input type="number" placeholder="지출 금액" />
+        </label>
+        <label>
+          내용
+          <input type="text" placeholder="지출 내용" />
+        </label>
+        <button>저장</button>
+      </CreateStyle>
+      <ListStyle>
+        <div>
+          <Link to={`/expenses/1`}>asdf</Link>
+        </div>
+        <div>asdf</div>
+        <div>asdf</div>
+        <div>asdf</div>
+        <div>asdf</div>
+      </ListStyle>
+    </Background>
+  );
 };
 
 export default Home;
